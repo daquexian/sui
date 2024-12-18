ede03708ccb832fad5ecaa3ce9b93f0e6ef8c55a
Mark Logan <103447440+mystenmark@users.noreply.github.com>
2024-12-18 15:06:37 -0800

    rebase fixes

--- crates/sui-core/src/authority/authority_per_epoch_store.rs
+++ crates/sui-core/src/authority/authority_per_epoch_store.rs
@@ -2440,6 +2440,7 @@ impl AuthorityPerEpochStore {
         }
     }
 
+    #[cfg(test)]
     pub fn test_insert_user_signature(
         &self,
         digest: TransactionDigest,
@@ -2458,6 +2459,13 @@ impl AuthorityPerEpochStore {
             .write()
             .push_consensus_output(output);
         self.consensus_notify_read.notify(&key, &());
+    }
+
+    #[cfg(test)]
+    pub(crate) fn push_consensus_output_for_tests(&self, output: ConsensusCommitOutput) {
+        self.consensus_quarantine
+            .write()
+            .push_consensus_output(output);
     }
 
     fn finish_consensus_certificate_process_with_batch(
@@ -4842,7 +4850,6 @@ impl ConsensusCommitOutput {
     }
 
     // in testing code we often need to write to the db outside of a consensus commit
-    #[cfg(test)]
     pub(crate) fn set_default_commit_stats_for_testing(&mut self) {
         self.record_consensus_commit_stats(Default::default());
     }
